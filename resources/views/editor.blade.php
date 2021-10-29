04c51ac7138a86e3f7ca109c9fa95e952f74a709
ZOHRE AZIZI <zohreh.azizi4@gmail.com>
2021-10-29 18:35:40 +0330

    fetch users data with ajax

--- resources/views/editor.blade.php
+++ resources/views/editor.blade.php
@@ -1,37 +1,43 @@
 @extends('layouts.dashboard')
 @section('title','admin dashboard')
 @section('content')
-
-    <div class="container-fluid">
-        <div class="alert alert-success" style="display:none"></div>
-        <div class="row">
-            <div class="col-lg-12">
-                @if(count($errors) > 0)
-                    @foreach($errors->all() as $error)
-                        <p class="alert alert-danger">{{$error}}</p>
-                    @endforeach
-                @endif
-            </div>
-            <div class="col-xl-12 col-lg-12 ">
-                <form action="{{route('content')}}" method="post">
-                    @csrf
-                    <textarea class="form-control" id="ckeditor" name="ckeditor"></textarea>
-                    <br>
-                    <div class="input-group mb-3">
-                        <span class="input-group-text" id="inputGroup-sizing-default">Title</span>
-                        <input id="title" type="text" name="title" class="form-control"
-                               aria-label="Sizing example input"
-                               aria-describedby="inputGroup-sizing-default">
-                    </div>
-                    <button type="submit" class="btn btn-primary" id="ajaxSubmit">Post</button>
-                    {{--                    //move to public--}}
-                    <script src="//cdn.ckeditor.com/4.16.2/standard/ckeditor.js"></script>
-                    <script>
-                        CKEDITOR.replace('ckeditor');
-                    </script>
-                </form>
+    <div class="row">
+        <div class="container-fluid col-lg-6">
+            <div class="alert alert-success" style="display:none"></div>
+            <div class="row">
+                <div class="col-lg-12">
+                    @if(count($errors) > 0)
+                        @foreach($errors->all() as $error)
+                            <p class="alert alert-danger">{{$error}}</p>
+                        @endforeach
+                    @endif
+                </div>
+                <div class="col-xl-12 col-lg-12 ">
+                    <form action="{{route('content')}}" method="post">
+                        @csrf
+                        <div class="input-group mb-3">
+                            <span class="input-group-text" id="inputGroup-sizing-default">Title</span>
+                            <input id="title" type="text" name="title" class="form-control"
+                                   aria-label="Sizing example input"
+                                   aria-describedby="inputGroup-sizing-default">
+                        </div>
+                        <textarea class="form-control" id="ckeditor" name="ckeditor"></textarea>
+                        <br>
+                        <button type="submit" class="btn btn-primary" id="ajaxSubmit">Post</button>
+                        {{--                    //move to public--}}
+                        <script src="//cdn.ckeditor.com/4.16.2/standard/ckeditor.js"></script>
+                        <script>
+                            CKEDITOR.replace('ckeditor');
+                        </script>
+                    </form>
+                </div>
             </div>
         </div>
+        <div class="container-fluid col-lg-5 border border-danger " >
+            <p id="formRequestErrors"></p>
+        </div>
+
+
     </div>
     <script src="http://code.jquery.com/jquery-3.3.1.min.js"
             integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
@@ -63,12 +69,13 @@
                         jQuery('.alert').html(result.success);
                     },
                     error: function (XMLHttpRequest, textStatus, errorThrown) {
-                        console.log('AJAX error:', XMLHttpRequest)
-                    }
+                        document.getElementById('formRequestErrors').innerHTML = XMLHttpRequest.responseText;
+                    },
 
                 });
 
             });
         });
+
     </script>
 @endsection
